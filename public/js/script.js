3bd5d79b601b52b6ccd59b2c0b931323f1a8fdf0
valholic <yansenlodova@gmail.com>
2024-04-13 19:53:09 +0700

    ini deh lah

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,25 @@
-// Navbar Fixed
+// Navbar Fixed & Back to top
+const backToTop = document.querySelector('.back-to-top');
+
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
+    backToTop.classList.add('opacity-70');
+
     if(window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
+        backToTop.classList.add('flex');
+        backToTop.classList.remove('hidden');
     } else {
         header.classList.remove('navbar-fixed');
+        backToTop.classList.remove('flex');
+        backToTop.classList.add('hidden');
     }
+}
+
+window.onscrollend = function() {
+    backToTop.classList.remove('opacity-70');
 }
 
 // Hamburger Menu
@@ -17,4 +29,30 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+// Ketika diklik di luar 
+window.addEventListener('click', function(e) {
+    if(e.target != navMenu && e.target != hamburger) {
+        navMenu.classList.add('hidden');
+        hamburger.classList.remove('hamburger-active');
+    }
+});
+
+// Dark Mode
+const mode = document.querySelector('#toggle-mode');
+const html = document.querySelector('html');
+
+if(localStorage.theme == 'dark') {
+    mode.checked = true;
+}
+
+mode.addEventListener('click', function() {
+    if(mode.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'selector',
   theme: {
     container: {
       center: true,
